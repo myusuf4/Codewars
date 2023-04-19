bbd8ad010d6e824bc118dafe252028e29aa13f9e
myusuf4 <nurmurzayevmuhammad@gmail.com>
2023-04-19 18:45:00 +0500

    tub son

--- 041923/index.js
+++ 041923/index.js
@@ -1,19 +1,46 @@
 // https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/javascript
-function fixTheMeerkat(arr) {
- return arr.reverse()
-}
-console.log(fixTheMeerkat(["arr","kiww"]));
+// function fixTheMeerkat(arr) {
+//  return arr.reverse()
+// }
+// console.log(fixTheMeerkat(["arr","kiww"]));
 
 
 
 // https://www.codewars.com/kata/5f70c883e10f9e0001c89673/train/javascript
-const flip=(d, a)=>{
-    let res=[];
-    if(d=="R"){
-     res= a.sort((b,c)=>b-c)
-    }
-    else {
-    res = a.sort((b,c)=>c-b)
-    }
-    return res
-  }
+// const flip=(d, a)=>{
+//     let res=[];
+//     if(d=="R"){
+//      res= a.sort((b,c)=>b-c)
+//     }
+//     else {
+//     res = a.sort((b,c)=>c-b)
+//     }
+//     return res
+//   }
+
+
+// let num =100;
+
+// for(let i = 1;i < num; i++){
+//     tub=true;
+//     for(let j = 2; j < i ; j++){
+//         if(i%j==0){
+//             tub=false;
+//             break;
+//         }
+//     }
+//     if(tub==true){
+//         console.log(`${i} => tub son`);
+//     }
+// }
+
+
+// let n=Number.parseInt(Math.random()*10000);
+
+// console.log(n);
+// if(n5000){
+//     console.log("qimmat");
+// }
+// else{
+//     console.log("arzon");
+// }
