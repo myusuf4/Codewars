290ecc198f156b695253f2517e9268ef88c35308
myusuf4 <nurmurzayevmuhammad@gmail.com>
2023-05-19 21:13:26 +0500

    solution

--- 051923/index.js
+++ 051923/index.js
@@ -23,3 +23,22 @@
 //   }
 
 // console.log(removeSmallest([4,6,7,1]));
+
+
+
+// https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/javascript
+
+// function solution(str){
+//     let i=0;
+//    let result=[];
+//    if(str.length%2!==0){
+//      str+="_"
+//    }
+//    while(i<str.length){
+//      result.push(str[i]+str[i+1])
+//      i+=2
+//    }
+//    return result
+//  }
+
+// console.log(solution("abc"));
