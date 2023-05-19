525e83f2ec413ba819d745eef4e792ba74646af9
myusuf4 <nurmurzayevmuhammad@gmail.com>
2023-05-19 20:53:44 +0500

    validatePin

--- 051923/index.js
+++ 051923/index.js
@@ -1,5 +1,16 @@
-function alphabetPosition(text) {
-    return text.toLowerCase().split('').map(a => a.charCodeAt(0) - 96).filter(a => a > 0 && a < 27).join(' ');
-  }
+// function alphabetPosition(text) {
+//     return text.toLowerCase().split('').map(a => a.charCodeAt(0) - 96).filter(a => a > 0 && a < 27).join(' ');
+//   }
 
-  console.log(alphabetPosition("The sun"));
+//   console.log(alphabetPosition("The sun"));
+
+
+//   https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/train/javascript
+
+function validatePIN (pin) {
+    for (let i of pin) {
+        if (!isFinite(parseInt(i))) return false;
+    }
+    return pin.length === 4 || pin.length === 6;
+}
+  console.log(validatePIN("1111"));
