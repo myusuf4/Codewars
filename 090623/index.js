db8edea601d32c39ee3172fe44c00822c649042f
Muhammad <121952755+myusuf4@users.noreply.github.com>
2023-06-09 21:50:32 +0500

    moveZeroes

---
+++ 090623/index.js
@@ -0,0 +1,6 @@
+var moveZeros = function (arr) {
+  let zero = arr.filter(el => el === 0);
+  let arrWithoutZero = arr.filter(el=> el !== 0)
+  return arrWithoutZero.concat(zero);
+}
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]));
